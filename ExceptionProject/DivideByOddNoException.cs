a7041f4aa73697c1fb681b0720497bbb9e7b0a4f
sameer singh <v-samesingh@microsoft.com>
2024-07-01 18:13:53 +0530

    Add project files.

---
+++ ExceptionProject/DivideByOddNoException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionProject
+{
+     class DivideByOddNoException:ApplicationException
+    {
+        public override string Message
+        {
+            get
+            {
+                return "Divisor should not be odd number";
+            }
+        }
+    }
+}
---
+++ ExceptionProject/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace ExceptionProject
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.Write("Enter 1st Number: ");
+                int x = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter 2nd Number: ");
+                int y = Convert.ToInt32(Console.ReadLine());
+                if (y % 2 != 0)
+                {
+                    //throw new ApplicationException("Divisor should not be odd number");
+                    throw new DivideByOddNoException();
+                }
+                int z = x / y;
+
+                Console.WriteLine(z);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+               
+            }
+            finally
+            {
+                Console.WriteLine("Finally Block is executed.");
+            }
+            Console.WriteLine("End of the Program");
+
+
+
+
+        }
+    }
+}
